347d580281a9784cfafdfec006605b34d7c8eb5b
李文峰 <2380945849@qq.com>
2021-06-12 21:22:03 +0800

    由父类IPointCluster计算AABB

--- AutoRetouch/BinaryClassifierAlg.cpp
+++ AutoRetouch/BinaryClassifierAlg.cpp
@@ -75,7 +75,7 @@ pcl::IndicesPtr CBinaryClassifierAlg::__getUnknownIndices()
 //FUNCTION: 
 SBox CBinaryClassifierAlg::__createExecuteArea() const
 {
-	auto AreaBox = CPointClusterSet::getInstance()->getAreaBox();
+	auto& AreaBox = CPointClusterSet::getInstance()->getAreaBox();
 	auto& SceneBox = CPointCloudAutoRetouchScene::getInstance()->getSceneAABB();
 	//TODO：添加至配置文件，可执行区域外沿的宽度与场景大小的比例
 	const Eigen::Vector3f Padding = (SceneBox.Max - SceneBox.Min) * 0.15f;
