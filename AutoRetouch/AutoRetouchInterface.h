3ce1fc69b21113cadc32fa00f2590fa42c602573
bowen tan <t519803253@163.com>
2021-06-15 10:13:04 +0800

    add outlier

--- AutoRetouch/AutoRetouchInterface.h
+++ AutoRetouch/AutoRetouchInterface.h
@@ -8,6 +8,7 @@
 #include "RegionGrowingByColorAlg.h"
 #include "BinaryClassifierAlg.h"
 #include "MaxVisibilityClusterAlg.h"
+#include "OutlierDetectingAlg.h"
 #include "CompositeClassifier.h"
 #include "CompositeBinaryClassifierAlg.h"
 
@@ -19,6 +20,8 @@ namespace hiveObliquePhotography
 		AUTORETOUCH_DECLSPEC void hiveUndoLastOp();
 
 		AUTORETOUCH_DECLSPEC void hiveGetGlobalPointLabelSet(std::vector<EPointLabel>& voGlobalLabel);
+
+		AUTORETOUCH_DECLSPEC void hiveGetIndicesByLabel(pcl::Indices& voPointIndices, EPointLabel vExpectLabel);
 
 		AUTORETOUCH_DECLSPEC void hiveResetSceneSelectStatus();
 
@@ -35,5 +38,7 @@ namespace hiveObliquePhotography
 		AUTORETOUCH_DECLSPEC bool hiveExecuteMaxVisibilityClustering(const pcl::IndicesPtr& vioPointIndices, EPointLabel vExpectLabel, const Eigen::Vector3f& vCameraPos, const Eigen::Matrix4d& vPvMatrix);
 
 		AUTORETOUCH_DECLSPEC bool hiveExecuteRegionGrowingByColor(const pcl::Indices& vPointIndices, EPointLabel vExpectLabel);
+
+		AUTORETOUCH_DECLSPEC bool hiveExecuteOutlierDetecting(pcl::Indices& vioPointIndices, EPointLabel vExpectLabel);
 	}
 }
