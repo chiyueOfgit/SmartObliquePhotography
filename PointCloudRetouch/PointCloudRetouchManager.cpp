4d0ed746c68e2fcaf4d29be1e1c12a0f8ab5de1f
yanci <yczhang@scu.edu.cn>
2021-06-21 09:31:47 +0800

    add parameters vSeedClusterIndex to buildNeighborhood()

--- PointCloudRetouch/PointCloudRetouchManager.cpp
+++ PointCloudRetouch/PointCloudRetouchManager.cpp
@@ -85,7 +85,7 @@ bool CPointCloudRetouchManager::executeMarker(const std::vector<pcl::index_t>& v
 
 //*****************************************************************
 //FUNCTION: 
-void CPointCloudRetouchManager::buildNeighborhood(pcl::index_t vSeed, std::vector<pcl::index_t>& voNeighborhood)
+void CPointCloudRetouchManager::buildNeighborhood(pcl::index_t vSeed, std::uint32_t vSeedClusterIndex, std::vector<pcl::index_t>& voNeighborhood)
 {
-	m_pNeighborhoodBuilder->buildNeighborhood(vSeed, voNeighborhood);
+	m_pNeighborhoodBuilder->buildNeighborhood(vSeed, vSeedClusterIndex, voNeighborhood);
 }
