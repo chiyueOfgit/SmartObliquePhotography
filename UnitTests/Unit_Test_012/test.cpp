e2bbe7681992b594f2e83aa57c946d8fb7587856
bowen tan <t519803253@163.com>
2021-06-22 10:03:52 +0800

    fix

--- UnitTests/Unit_Test_012/test.cpp
+++ UnitTests/Unit_Test_012/test.cpp
@@ -3,6 +3,7 @@
 #include "PointCluster.h"
 #include "pcl/io/pcd_io.h"
 #include "InitialClusterCreator.h"
+#include "PointCloudRetouchConfig.h"
 
 
 //≤‚ ‘”√¿˝¡–±Ì£∫
@@ -17,25 +18,53 @@ const std::string ConfigPath = "PointCloudRetouchConfig.xml";
 class TestPointCluster : public testing::Test
 {
 public:
-    hiveConfig::CHiveConfig* Config;
-
+    hiveConfig::CHiveConfig* pConfig = new PointCloudRetouch::CPointCloudRetouchConfig;
+	const hiveConfig::CHiveConfig* pClusterConfig = nullptr;
 protected:
 	
 	void SetUp() override
 	{
-		if (hiveConfig::hiveParseConfig(ConfigPath, hiveConfig::EConfigType::XML, Config)!= hiveConfig::EParseResult::SUCCEED)
+		if (hiveConfig::hiveParseConfig(ConfigPath, hiveConfig::EConfigType::XML, pConfig)!= hiveConfig::EParseResult::SUCCEED)
 		{
 			_HIVE_OUTPUT_WARNING(_FORMAT_STR1("Failed to parse config file [%1%].", ConfigPath));
 			return;
 		}
 		PointCloud_t::Ptr pCloud(new PointCloud_t);
 		pcl::io::loadPCDFile("slice 3.pcd", *pCloud);
-		PointCloudRetouch::hiveInit(pCloud, Config);
+		PointCloudRetouch::hiveInit(pCloud, pConfig);
+
+		for (auto i = 0; i < pConfig->getNumSubconfig(); i++)
+		{
+			const hiveConfig::CHiveConfig* TempConfig = pConfig->getSubconfigAt(i);
+			if (_IS_STR_IDENTICAL(TempConfig->getSubconfigType(), std::string("TASK")))
+			{
+				for (auto k = 0; k < TempConfig->getNumSubconfig(); k++)
+				{
+					const hiveConfig::CHiveConfig* cConfig = pConfig->getSubconfigAt(k);
+					if (_IS_STR_IDENTICAL(cConfig->getSubconfigType(), std::string("CLUSTER")))
+					{
+						if (!pClusterConfig)
+				        {
+					        pClusterConfig = cConfig;
+				        }
+				        else
+				        {
+					       _HIVE_OUTPUT_WARNING("It is NOT allowed to define cluster twice.");
+				        }
+					}
+					continue;
+					
+				}
+				
+				continue;
+			}
+		}
 	}
 
 	void TearDown() override
 	{
-		delete Config;
+		delete pConfig;
+		delete pClusterConfig;
 	}
 };
 TEST_F(TestPointCluster, DeathTest_Uninitialized)
@@ -53,8 +82,7 @@ TEST_F(TestPointCluster, DeathTest_InvalidIndex)
 	pcl::index_t TestIndex = -1;
 	Eigen::Matrix4d Pv;
 	PointCloudRetouch::CInitialClusterCreator Creator;
-	PointCloudRetouch::CPointCluster *PointCluster;
-	PointCluster = Creator.createInitialCluster(UserMarkedRegion, 0.8, 10, PointCloudRetouch::EPointLabel::KEPT, { 400,400 }, Pv, {1000,800},Config);
+	auto PointCluster = Creator.createInitialCluster(UserMarkedRegion, 0.8, 10, PointCloudRetouch::EPointLabel::KEPT, { 400,400 }, Pv, {1000,800}, pClusterConfig);
 	ASSERT_ANY_THROW(double Res = PointCluster->evaluateProbability(TestIndex));
 }
 
@@ -65,9 +93,8 @@ TEST_F(TestPointCluster, FalseProbability_Test)
 	
 	pcl::index_t TestIndex = 1;
 	Eigen::Matrix4d Pv;
-	PointCloudRetouch::CPointCluster *PointCluster;
 	PointCloudRetouch::CInitialClusterCreator Creator;
-	PointCluster = Creator.createInitialCluster(UserMarkedRegion, 0.8, 10, PointCloudRetouch::EPointLabel::KEPT, { 400,400 }, Pv, { 1000,800 }, Config);
+	auto PointCluster = Creator.createInitialCluster(UserMarkedRegion, 0.8, 10, PointCloudRetouch::EPointLabel::KEPT, { 400,400 }, Pv, { 1000,800 }, pClusterConfig);
 	double Res = PointCluster->evaluateProbability(TestIndex);
 	EXPECT_LE(Res, 1.0);
 	EXPECT_GE(Res, 0.0);
