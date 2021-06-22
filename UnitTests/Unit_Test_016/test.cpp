e35d1661d790fe65e2ec30fe218ec8476286c0bb
李文峰 <2380945849@qq.com>
2021-06-22 10:17:40 +0800

    update

--- UnitTests/Unit_Test_016/test.cpp
+++ UnitTests/Unit_Test_016/test.cpp
@@ -7,7 +7,7 @@
 
 //	测试用例列表：
 //	* Illegal_Input_Test：测试输入非法时能否正确报错，报错后能否继续正确运行
-//	* Symmetry_Test：邻居关系具有对称性，即若A是B的邻居，则B一定是A的邻居
+//	* Symmetry_Test：邻居关系具有对称性，即若A是B的邻居，则B一定是A的邻居 FIXME: 邻居关系似乎、好像、也许，并没有对称性
 //	* Anti_Reflexive_Test：邻居关系具有反自反性，即A一定不是A的邻居
 //	* 暂时只测试INeighborhoodBuilder基类的功能
 
@@ -54,7 +54,7 @@ TEST_F(CTestNeighborhoodBuilder, Illegal_Input_Test)
 	std::vector<pcl::index_t> Neighborhood;
 	ASSERT_NO_THROW(pManager->buildNeighborhood(50, 1, Neighborhood));
 	ASSERT_ANY_THROW(pManager->buildNeighborhood(-1, 1, Neighborhood));
-	ASSERT_ANY_THROW(pManager->buildNeighborhood(999, 1, Neighborhood));
+	ASSERT_ANY_THROW(pManager->buildNeighborhood(1000000, 1, Neighborhood));
 }
 
 TEST_F(CTestNeighborhoodBuilder, Symmetry_Test)
@@ -64,14 +64,14 @@ TEST_F(CTestNeighborhoodBuilder, Symmetry_Test)
 	for (size_t i = 0; i < 500; i++)
 	{
 		auto TestIndex = hiveMath::hiveGenerateRandomInteger(0, 999);
-
+		
 		std::vector<pcl::index_t> Neighborhood;
-		pManager->buildNeighborhood(TestIndex, 0, Neighborhood);
-		auto Neighbor = Neighborhood.front();
-
-		pManager->buildNeighborhood(Neighbor, 0, Neighborhood);
+		pManager->buildNeighborhood(TestIndex, 1, Neighborhood);
+		auto Neighbor = Neighborhood.back();
+		
+		pManager->buildNeighborhood(Neighbor, 1, Neighborhood);
 		auto k = Neighborhood.begin();
-		for (; k != Neighborhood.end(); k++)
+		for (; k != Neighborhood.end(); ++k)
 			if (*k == TestIndex)
 				break;
 
@@ -88,7 +88,7 @@ TEST_F(CTestNeighborhoodBuilder, Anti_Reflexive_Test)
 		auto TestIndex = hiveMath::hiveGenerateRandomInteger(0, 999);
 
 		std::vector<pcl::index_t> Neighborhood;
-		pManager->buildNeighborhood(TestIndex, 0, Neighborhood);
+		pManager->buildNeighborhood(TestIndex, 1, Neighborhood);
 
 		auto k = Neighborhood.begin();
 		for (; k != Neighborhood.end(); k++)
