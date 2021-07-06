4465c562fb449ee5dfb1681ccd14154f22f254df
Medjed <18181362690@163.com>
2021-07-06 14:47:11 +0800

    .

--- PointCloudRetouch/ColorFeature.cpp
+++ PointCloudRetouch/ColorFeature.cpp
@@ -2,6 +2,10 @@
 #include "ColorFeature.h"
 #include "PointCloudRetouchManager.h"
 
+#define gamma(x) (x > 0.04045 ? pow((x + 0.055f) / 1.055f, 2.4f) : x / 12.92)
+#define deg2Rad(deg) (deg * (M_PI / 180.0))
+#define rad2Deg(rad) ((180.0 / M_PI) * rad)
+
 using namespace hiveObliquePhotography::PointCloudRetouch;
 
 _REGISTER_EXCLUSIVE_PRODUCT(CColorFeature, KEYWORD::COLOR_FEATURE)
@@ -12,12 +16,12 @@ double CColorFeature::generateFeatureV(const std::vector<pcl::index_t>& vDetermi
 {
 	_ASSERTE(m_pConfig);
 	m_ColorThreshold = *m_pConfig->getAttribute<float>("COLOR_THRESHOLD");
-	m_NumMainColors = *m_pConfig->getAttribute<int>("NUM_MAIN_COLORS");
+	m_MaxNumMainColors = *m_pConfig->getAttribute<int>("NUM_MAIN_COLORS");
 	
 	if (vDeterminantPointSet.empty() || vValidationSet.empty())
 		return 0.0;
 
-	__computeMainColors(vDeterminantPointSet, m_MainBaseColors, m_NumMainColors);
+	__computeMainColors(vDeterminantPointSet, m_MainBaseColors, m_MaxNumMainColors);
 
 	double Score = 0.0;
 	for (auto ValidationIndex : vValidationSet)
@@ -59,7 +63,7 @@ void CColorFeature::__computeMainColors(const std::vector<pcl::index_t>& vPointI
 
 //*****************************************************************
 //FUNCTION: 
-std::vector<Eigen::Vector3i> CColorFeature::__kMeansCluster(const std::vector<Eigen::Vector3i>& vColorSet, std::size_t vK) const
+std::vector<Eigen::Vector3i> CColorFeature::__kMeansCluster(const std::vector<Eigen::Vector3i>& vColorSet, std::size_t vMaxK) const
 {
 	std::vector<std::pair<Eigen::Vector3i*, Eigen::Vector3i>> TagAndColorSet(vColorSet.size(), { nullptr, Eigen::Vector3i() });
 	for (size_t i = 0; i < TagAndColorSet.size(); i++)
@@ -68,7 +72,7 @@ std::vector<Eigen::Vector3i> CColorFeature::__kMeansCluster(const std::vector<Ei
 		Color = vColorSet[i];
 	}
 	
-	std::vector<Eigen::Vector3i> ClusterCentroids(vK, Eigen::Vector3i());
+	std::vector<Eigen::Vector3i> ClusterCentroids(vMaxK, Eigen::Vector3i());
 	for (auto& Centroid : ClusterCentroids)
 		Centroid = TagAndColorSet[hiveMath::hiveGenerateRandomInteger(std::size_t(0), TagAndColorSet.size() - 1)].second;
 
@@ -110,4 +114,144 @@ std::vector<Eigen::Vector3i> CColorFeature::__kMeansCluster(const std::vector<Ei
 	}
 	
 	return ClusterCentroids;
+}
+
+float CColorFeature::__calcColorDifferences(const Eigen::Vector3i& vLColor, const Eigen::Vector3i& vRColor)
+{
+    Eigen::Vector3f LRGBColor{ static_cast<float>(vLColor[0]),static_cast<float>(vLColor[1]),static_cast<float>(vLColor[2]) };
+    Eigen::Vector3f RRGBColor{ static_cast<float>(vRColor[0]),static_cast<float>(vRColor[1]),static_cast<float>(vRColor[2]) };
+    return __calculateCIEDE2000(__RGB2LAB(LRGBColor), __RGB2LAB(RRGBColor));
+}
+
+float CColorFeature::__calculateCIEDE2000(const LAB& lab1, const LAB& lab2)
+{
+    const float k_L = 1.0, k_C = 1.0, k_H = 1.0;
+    const float deg360InRad = deg2Rad(360.0);
+    const float deg180InRad = deg2Rad(180.0);
+    const float pow25To7 = 6103515625.0;
+
+    float C1 = sqrt((lab1.a * lab1.a) + (lab1.b * lab1.b));
+    float C2 = sqrt((lab2.a * lab2.a) + (lab2.b * lab2.b));
+
+    float barC = (C1 + C2) / 2.0;
+
+    float G = 0.5 * (1 - sqrt(pow(barC, 7) / (pow(barC, 7) + pow25To7)));
+
+    float a1Prime = (1.0 + G) * lab1.a;
+    float a2Prime = (1.0 + G) * lab2.a;
+
+    float CPrime1 = sqrt((a1Prime * a1Prime) + (lab1.b * lab1.b));
+    float CPrime2 = sqrt((a2Prime * a2Prime) + (lab2.b * lab2.b));
+
+    float hPrime1;
+    if (lab1.b == 0 && a1Prime == 0)
+        hPrime1 = 0.0;
+    else {
+        hPrime1 = atan2(lab1.b, a1Prime);
+
+        if (hPrime1 < 0)
+            hPrime1 += deg360InRad;
+    }
+    float hPrime2;
+    if (lab2.b == 0 && a2Prime == 0)
+        hPrime2 = 0.0;
+    else {
+        hPrime2 = atan2(lab2.b, a2Prime);
+
+        if (hPrime2 < 0)
+            hPrime2 += deg360InRad;
+    }
+
+    float deltaLPrime = lab2.l - lab1.l;
+
+    float deltaCPrime = CPrime2 - CPrime1;
+
+    float deltahPrime;
+    float CPrimeProduct = CPrime1 * CPrime2;
+    if (CPrimeProduct == 0)
+        deltahPrime = 0;
+    else {
+
+        deltahPrime = hPrime2 - hPrime1;
+        if (deltahPrime < -deg180InRad)
+            deltahPrime += deg360InRad;
+        else if (deltahPrime > deg180InRad)
+            deltahPrime -= deg360InRad;
+    }
+
+    float deltaHPrime = 2.0 * sqrt(CPrimeProduct) *
+        sin(deltahPrime / 2.0);
+
+    float barLPrime = (lab1.l + lab2.l) / 2.0;
+
+    float barCPrime = (CPrime1 + CPrime2) / 2.0;
+
+    float barhPrime, hPrimeSum = hPrime1 + hPrime2;
+    if (CPrime1 * CPrime2 == 0) {
+        barhPrime = hPrimeSum;
+    }
+    else {
+        if (fabs(hPrime1 - hPrime2) <= deg180InRad)
+            barhPrime = hPrimeSum / 2.0;
+        else {
+            if (hPrimeSum < deg360InRad)
+                barhPrime = (hPrimeSum + deg360InRad) / 2.0;
+            else
+                barhPrime = (hPrimeSum - deg360InRad) / 2.0;
+        }
+    }
+
+    float T = 1.0 - (0.17 * cos(barhPrime - deg2Rad(30.0))) +
+        (0.24 * cos(2.0 * barhPrime)) +
+        (0.32 * cos((3.0 * barhPrime) + deg2Rad(6.0))) -
+        (0.20 * cos((4.0 * barhPrime) - deg2Rad(63.0)));
+
+    float deltaTheta = deg2Rad(30.0) *
+        exp(-pow((barhPrime - deg2Rad(275.0)) / deg2Rad(25.0), 2.0));
+
+    float R_C = 2.0 * sqrt(pow(barCPrime, 7.0) /
+        (pow(barCPrime, 7.0) + pow25To7));
+
+    float S_L = 1 + ((0.015 * pow(barLPrime - 50.0, 2.0)) /
+        sqrt(20 + pow(barLPrime - 50.0, 2.0)));
+
+    float S_C = 1 + (0.045 * barCPrime);
+
+    float S_H = 1 + (0.015 * barCPrime * T);
+
+    float R_T = (-sin(2.0 * deltaTheta)) * R_C;
+
+    float deltaE = sqrt(
+        pow(deltaLPrime / (k_L * S_L), 2.0) +
+        pow(deltaCPrime / (k_C * S_C), 2.0) +
+        pow(deltaHPrime / (k_H * S_H), 2.0) +
+        (R_T * (deltaCPrime / (k_C * S_C)) * (deltaHPrime / (k_H * S_H))));
+
+    return (deltaE);
+}
+
+LAB CColorFeature::__RGB2LAB(const Eigen::Vector3f& vRGBColor)
+{
+    float B = gamma(vRGBColor[2] / 255.0f);
+    float G = gamma(vRGBColor[1] / 255.0f);
+    float R = gamma(vRGBColor[0] / 255.0f);
+
+    float X = 0.412453 * R + 0.357580 * G + 0.180423 * B;
+    float Y = 0.212671 * R + 0.715160 * G + 0.072169 * B;
+    float Z = 0.019334 * R + 0.119193 * G + 0.950227 * B;
+
+    X /= 0.95047;
+    Y /= 1.0;
+    Z /= 1.08883;
+
+    float FX = X > 0.008856f ? pow(X, 1.0f / 3.0f) : (7.787f * X + 0.137931f);
+    float FY = Y > 0.008856f ? pow(Y, 1.0f / 3.0f) : (7.787f * Y + 0.137931f);
+    float FZ = Z > 0.008856f ? pow(Z, 1.0f / 3.0f) : (7.787f * Z + 0.137931f);
+
+    LAB Lab;
+    Lab.l = Y > 0.008856f ? (116.0f * FY - 16.0f) : (903.3f * Y);
+    Lab.a = 500.f * (FX - FY);
+    Lab.b = 200.f * (FY - FZ);
+
+    return Lab;
 }
--- PointCloudRetouch/ColorFeature.h
+++ PointCloudRetouch/ColorFeature.h
@@ -5,6 +5,13 @@ namespace hiveObliquePhotography
 {
 	namespace PointCloudRetouch
 	{
+        struct LAB
+        {
+            float l;
+            float a;
+            float b;
+        };
+
 		class CColorFeature : public IFeature
 		{
 		public:
@@ -13,18 +20,23 @@ namespace hiveObliquePhotography
 
 			virtual double generateFeatureV(const std::vector<pcl::index_t>& vDeterminantPointSet, const std::vector<pcl::index_t>& vValidationSet, pcl::index_t vClusterCenter) override;
 			virtual double evaluateFeatureMatchFactorV(pcl::index_t vInputPoint) override;
+
 #ifdef _UNIT_TEST
 			std::vector<Eigen::Vector3i> kMeansCluster(const std::vector<Eigen::Vector3i>& vColorSet, std::size_t vK) { return __kMeansCluster(vColorSet, vK); };
 #endif
 		private:
-			void __computeMainColors(const std::vector<pcl::index_t>& vPointIndices, std::vector<Eigen::Vector3i>& vMainColors, std::size_t vK);
+			void __computeMainColors(const std::vector<pcl::index_t>& vPointIndices, std::vector<Eigen::Vector3i>& vMainColors, std::size_t vMaxK);
 			std::vector<Eigen::Vector3i> __kMeansCluster(const std::vector<Eigen::Vector3i>& vColorSet, std::size_t vK) const;
+
+			float __calcColorDifferences(const Eigen::Vector3i& vLColor, const Eigen::Vector3i& vRColor);
+			float __calculateCIEDE2000(const LAB& lab1, const LAB& lab2);
+			LAB __RGB2LAB(const Eigen::Vector3f& vRGBColor);
 
 			PointCloud_t::Ptr m_pCloud = nullptr;
 			std::vector<Eigen::Vector3i> m_MainBaseColors;
 
-			float m_ColorThreshold = 30;
-			std::size_t m_NumMainColors = 3;
+			float m_ColorThreshold = 30.0f;
+			std::size_t m_MaxNumMainColors = 3;
 		};
 	}
 
