55a44f95013835ff99799ca104f5f50f4e7c8d40
bowen tan <t519803253@163.com>
2021-07-13 17:43:07 +0800

    lil change

--- PointCloudRetouch/NormalComplexity.h
+++ PointCloudRetouch/NormalComplexity.h
@@ -13,11 +13,20 @@ namespace hiveObliquePhotography
 			
 			double generateFeatureV(const std::vector<pcl::index_t>& vDeterminantPointSet, const std::vector<pcl::index_t>& vValidationSet, pcl::index_t vClusterCenter) override;
 			double evaluateFeatureMatchFactorV(pcl::index_t vInputPoint) override;
+			bool onProductCreatedV(const hiveConfig::CHiveConfig* vFeatureConfig)
+			{
+				_ASSERTE(vFeatureConfig);
+				m_pConfig = vFeatureConfig;
+
+				return true;	
+		    }
+			
 #ifdef _UNIT_TEST
 			double calcSinglePointNormalComplexity(pcl::index_t vIndex) { return __calcSinglePointNormalComplexity(vIndex); }
 #endif		
 		private:
 			float m_AverageDon;
+			pcl::search::KdTree<pcl::PointSurfel>::Ptr m_pTree = nullptr;
 			
 			float __calcSinglePointNormalComplexity(pcl::index_t vIndex);
 			float __calcPointCloudNormalComplexity(pcl::PointCloud<pcl::PointXYZ>::Ptr vCloud);
