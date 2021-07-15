1985466e3af86e6784bd7077ce874a1bd6d05bd2
Medjed <18181362690@163.com>
2021-07-15 20:19:23 +0800

    test010

--- PointCloudRetouch/PointCloudRetouchScene.h
+++ PointCloudRetouch/PointCloudRetouchScene.h
@@ -14,13 +14,13 @@ namespace hiveObliquePhotography
 			~CPointCloudRetouchScene();
 
 			void init(PointCloud_t::Ptr vPointCloudScene);
-			void reset(std::uint32_t vTimestamp) {};
+			void reset() { m_pPointCloudScene = nullptr; }
 
 			Eigen::Vector4f getPositionAt(pcl::index_t vIndex) const;
 			Eigen::Vector4f getNormalAt(pcl::index_t vIndex) const;
 			Eigen::Vector3i getColorAt(pcl::index_t vIndex) const;
 			
-			std::size_t getNumPoint() const { _ASSERTE(m_pPointCloudScene); return m_pPointCloudScene->size(); }
+			std::size_t getNumPoint() const { return m_pPointCloudScene ? m_pPointCloudScene->size() : 0; }
 
 		private:
 			Eigen::Vector3i __extractRgba(float vRgba) const;
