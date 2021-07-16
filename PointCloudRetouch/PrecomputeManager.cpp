92e328b74198f7b737854f8d5165632314287f3c
Medjed <18181362690@163.com>
2021-07-16 15:29:26 +0800

    .

--- PointCloudRetouch/PrecomputeManager.cpp
+++ PointCloudRetouch/PrecomputeManager.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "PrecomputeManager.h"
 
+
 using namespace hiveObliquePhotography::PointCloudRetouch;
 
 //*****************************************************************
--- PointCloudRetouch/PrecomputeManager.h
+++ PointCloudRetouch/PrecomputeManager.h
@@ -17,7 +17,26 @@ namespace hiveObliquePhotography
 			void clear();
 
 		private:
+			template<class T>
+			void __serialization(const std::string& vPath, const T& vContainer) const
+			{
+				std::ofstream file(vPath.c_str());
+				boost::archive::text_oarchive oa(file);
+				oa& BOOST_SERIALIZATION_NVP(vIndices);
+				file.close();
+			}
+
+			template<class T>
+			void __deserialization(const std::string& vPath, T& voContainer) const
+			{
+				std::ifstream file(vPath.c_str());
+				boost::archive::text_iarchive ia(file);
+				ia >> BOOST_SERIALIZATION_NVP(voContainer);
+				file.close();
+			}
+			
 			std::vector<std::function<bool()>> m_PrecomputeList;
+			
 		};
 	}
 }
