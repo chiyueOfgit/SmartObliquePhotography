7cd9061bcdffb706c9e06940312daa0ce72017bc
李文峰 <2380945849@qq.com>
2021-07-17 22:03:11 +0800

    丰富CPointCloudRetouchScene::dump

--- PointCloudRetouch/PointCloudRetouchScene.h
+++ PointCloudRetouch/PointCloudRetouchScene.h
@@ -23,26 +23,48 @@ namespace hiveObliquePhotography
 			std::size_t getNumPoint() const { return m_pPointCloudScene ? m_pPointCloudScene->size() : 0; }
 
 			template<typename Point_t>
+			void dumpPoint(pcl::index_t vIndex, Point_t& voPoint) const
+			{
+				_ASSERTE(Index < m_pPointCloudScene->size());
+				__dumpPoint(m_pPointCloudScene->points[vIndex], voPoint);
+			}
+			
+			template<typename Point_t>
+			void dumpPointCloud(pcl::PointCloud<Point_t>& voPointCloud) const
+			{
+				for (const auto& Point : *m_pPointCloudScene)
+				{
+					Point_t TempPoint;
+					__dumpPoint(Point, TempPoint);
+					voPointCloud.push_back(TempPoint);
+				}
+			}
+			
+			template<typename Point_t>
 			void dumpPointCloud(const std::vector<pcl::index_t>& vIndices, pcl::PointCloud<Point_t>& voPointCloud) const
 			{
 				for (auto Index : vIndices)
 				{
 					_ASSERTE(Index < m_pPointCloudScene->size());
-					Point_t Point;
-
-					if constexpr(pcl::traits::has_xyz_v<Point_t>)
-						memcpy(Point.data, m_pPointCloudScene->points[Index].data, sizeof(Point.data));
-					if constexpr (pcl::traits::has_normal_v<Point_t>)
-						memcpy(Point.data_n, m_pPointCloudScene->points[Index].data_n, sizeof(Point.data_n));
-					if constexpr (pcl::traits::has_color_v<Point_t>)
-						Point.rgba = m_pPointCloudScene->points[Index].rgba;
-
-					voPointCloud.push_back(Point);
+					Point_t TempPoint;
+					__dumpPoint(m_pPointCloudScene->points[Index], TempPoint);
+					voPointCloud.push_back(TempPoint);
 				}
 			}
 
 		private:
 			Eigen::Vector3i __extractRgba(float vRgba) const;
+
+			template<typename Point_t>
+			void __dumpPoint(const PointCloud_t::PointType& vSrc, Point_t& voDst) const
+			{
+				if constexpr (pcl::traits::has_xyz_v<Point_t>)
+					memcpy(voDst.data, vSrc.data, sizeof(vSrc.data));
+				if constexpr (pcl::traits::has_normal_v<Point_t>)
+					memcpy(voDst.data_n, vSrc.data_n, sizeof(vSrc.data_n));
+				if constexpr (pcl::traits::has_color_v<Point_t>)
+					voDst.rgba = vSrc.rgba;
+			}
 
 			PointCloud_t::Ptr m_pPointCloudScene = nullptr;
 		};
