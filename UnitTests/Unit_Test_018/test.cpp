fb450f1970d3707a437e9405a6486faf5432e9a6
bowen tan <t519803253@163.com>
2021-07-28 11:40:08 +0800

    fix

--- UnitTests/Unit_Test_018/test.cpp
+++ UnitTests/Unit_Test_018/test.cpp
@@ -80,7 +80,12 @@ int TestBoundaryDetection::m_TestNumber = -1;
 
 TEST_F(TestBoundaryDetection, Boundary_Detection_BaseTest_1)
 {
-	std::vector<pcl::index_t> BoundaryPoints;
+    std::vector<pcl::index_t> BoundaryPoints;
+	int Size = m_pManager->getRetouchScene().getNumPoint();
+	for (int i = 0; i < Size; i++)
+	{
+		BoundaryPoints.push_back(i);
+	}
 	m_pDetector->execute<CBoundaryDetector>(BoundaryPoints, m_pDetectorConfig);
 	ASSERT_TRUE(!BoundaryPoints.empty());
 
@@ -106,6 +111,12 @@ TEST_F(TestBoundaryDetection, Boundary_Detection_BaseTest_1)
 TEST_F(TestBoundaryDetection, Boundary_Detection_BaseTest_2)
 {
 	std::vector<pcl::index_t> BoundaryPoints;
+	int Size = m_pManager->getRetouchScene().getNumPoint();
+	for (int i = 0; i < Size; i++)
+	{
+		BoundaryPoints.push_back(i);
+	}
+	
 	m_pDetector->execute<CBoundaryDetector>(BoundaryPoints, m_pDetectorConfig);
 	ASSERT_TRUE(!BoundaryPoints.empty());
 
