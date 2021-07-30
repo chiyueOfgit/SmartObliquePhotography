72a28d2ad823ce6b65f401200401341d692224dd
Ecclesia <Ecclesia>
2021-07-30 16:21:35 +0800

    已合并 PR 36: 纹理合成

---
+++ PointCloudRetouch/TextureSynthesizer.h
@@ -0,0 +1,28 @@
+#pragma once
+namespace hiveObliquePhotography::PointCloudRetouch
+{
+	template <typename Color_t>
+	class CTextureSynthesizer
+	{
+	public:
+		using Texture_t = Eigen::Matrix<Color_t, -1, -1>;
+		using Feature_t = Eigen::Matrix<Color_t, -1, 1>;
+		
+		CTextureSynthesizer() = default;
+		~CTextureSynthesizer() = default;
+
+		void execute(const Texture_t& vInput, const Eigen::MatrixXi& vMask, Texture_t& vioScene);
+
+	private:
+		int m_KernelSize = 9;
+		std::vector<std::pair<int, int>> m_NeighborOffset;
+		
+		std::vector<std::pair<int, int>> __generateNeighborOffset(int vKernelSize) const;
+		Feature_t __generateFeatureAt(const Texture_t& vTexture, size_t vRowId, size_t vColId) const;
+		std::pair<Eigen::Index, Eigen::Index> __findNearestPos(const Texture_t& vTexture, const Feature_t& vFeature) const;
+		float __computeFeatureDistance(const Feature_t& vLhs, const Feature_t& vRhs) const;
+	};
+
+	template class CTextureSynthesizer<Eigen::Vector3i>;
+	template class CTextureSynthesizer<Eigen::Matrix<float, 1, 1>>;
+}
