e8b7dda94e86da2f9da9d0aa5949a91babdcb48d
李文峰 <2380945849@qq.com>
2021-08-02 17:10:04 +0800

    speed up

--- PointCloudRetouch/TextureSynthesizer.cpp
+++ PointCloudRetouch/TextureSynthesizer.cpp
@@ -15,44 +15,75 @@ void CTextureSynthesizer<Scalar_t, Channel>::execute(const Texture_t& vInput, co
 			if (vMask.coeff(RowId, ColId) != 0)
 			{
 				//生成邻居掩码
-				//根据邻居掩码生成Feature
 				const int KernelOffset = m_KernelSize / 2;
 				const int KernelWidth = KernelOffset * 2 + 1;
 				NeighborMask_t NeighborMask(KernelWidth, KernelWidth);
-				std::vector<Texture_t::value_type> Feature;
 				for (int i = -KernelOffset; i <= KernelOffset; ++i)
 					for (int k = -KernelOffset; k <= KernelOffset; ++k)
 					{
 						auto RowIdWithOffset = (i + RowId + vMask.rows()) % vMask.rows();
 						auto ColIdWithOffset = (k + ColId + vMask.cols()) % vMask.cols();
 
-						//TODO: 满足前者但不满足后者也不行
-						if (i < 0 || (i == 0 && k < 0) || vMask.coeff(RowIdWithOffset, ColIdWithOffset) == 0)
-						{
+						if (vMask.coeff(RowIdWithOffset, ColIdWithOffset) == 0)
 							NeighborMask(i + KernelOffset, k + KernelOffset) = 1;
-							Feature.emplace_back(vioScene.coeff(RowIdWithOffset, ColIdWithOffset));
+						else if (i < 0 || (i == 0 && k < 0))
+						{
+							if (i + RowId >= 0 && i + RowId < vMask.rows() && k + ColId >= 0 && k + ColId < vMask.cols())
+							{
+								NeighborMask(i + KernelOffset, k + KernelOffset) = 1;
+							}
+							else
+							{
+								NeighborMask(i + KernelOffset, k + KernelOffset) = 0;
+							}
 						}
 						else
+						{
 							NeighborMask(i + KernelOffset, k + KernelOffset) = 0;
+						}
 					}
 
+				auto Feature = __generateFeatureAt(vioScene, NeighborMask, RowId, ColId);
 				//根据邻居掩码和Feature __findNearestPos
-				auto [NearestRowId, NearestColId] = __findNearestPos(vInput, NeighborMask, __flatFeature(Feature));
+				auto [NearestRowId, NearestColId] = __findNearestPos(vInput, NeighborMask, Feature);
 				vioScene.coeffRef(RowId, ColId) = vInput.coeff(NearestRowId, NearestColId);
 			}
 }
 
 //*****************************************************************
+//FUNCTION:
+template <typename Scalar_t, unsigned Channel>
+Scalar_t CTextureSynthesizer<Scalar_t, Channel>::__computeDistance(const Feature_t& vLhs, const Feature_t& vRhs) const
+{
+	_ASSERTE(vLhs.size() == vRhs.size());
+	const auto Size = vLhs.size();
+	
+	Eigen::Matrix<Scalar_t, Eigen::Dynamic, 1> FlatFeature(Size * Channel);
+	for (size_t i = 0; i < Size; ++i)
+		for (unsigned k = 0; k < Channel; ++k)
+			FlatFeature((i * Channel) + k) = vLhs[i][k] - vRhs[i][k];
+
+	return FlatFeature.squaredNorm();
+}
+
+//*****************************************************************
 //FUNCTION: 
 template <typename Scalar_t, unsigned Channel>
-typename CTextureSynthesizer<Scalar_t, Channel>::Feature_t CTextureSynthesizer<Scalar_t, Channel>::__flatFeature(const std::vector<typename Texture_t::value_type>& vFeature) const
+typename CTextureSynthesizer<Scalar_t, Channel>::Feature_t CTextureSynthesizer<Scalar_t, Channel>::__generateFeatureAt(const Texture_t& vTexture, const NeighborMask_t& vNeighborMask, size_t vRowId, size_t vColId) const
 {
-	Feature_t FlatFeature(vFeature.size() * Channel);
-	for (size_t i = 0; i < vFeature.size(); ++i)
-		for (unsigned k = 0; k < Channel; ++k)
-			FlatFeature((i * Channel) + k) = vFeature[i][k];
+	const int KernelOffset = m_KernelSize / 2;
+	Feature_t Feature;
+	Feature.reserve((KernelOffset * 2 + 1) * (KernelOffset * 2 + 1));
+	for (int i = -KernelOffset; i <= KernelOffset; ++i)
+		for (int k = -KernelOffset; k <= KernelOffset; ++k)
+			if (vNeighborMask(i + KernelOffset, k + KernelOffset) != 0)
+			{
+				auto RowIdWithOffset = (i + vRowId + vTexture.rows()) % vTexture.rows();
+				auto ColIdWithOffset = (k + vColId + vTexture.cols()) % vTexture.cols();
+				Feature.emplace_back(vTexture.coeff(RowIdWithOffset, ColIdWithOffset));
+			}
 
-	return FlatFeature;
+	return Feature;
 }
 
 //*****************************************************************
@@ -65,21 +96,8 @@ std::pair<Eigen::Index, Eigen::Index> CTextureSynthesizer<Scalar_t, Channel>::__
 	for (Eigen::Index RowId = 0; RowId < vTexture.rows(); ++RowId)
 		for (Eigen::Index ColId = 0; ColId < vTexture.cols(); ++ColId)
 		{
-			//根据他人的邻居掩码生成Feature
-			const int KernelOffset = m_KernelSize / 2;
-			std::vector<Texture_t::value_type> Feature;
-			for (int i = -KernelOffset; i <= KernelOffset; ++i)
-				for (int k = -KernelOffset; k <= KernelOffset; ++k)
-				{
-					auto RowIdWithOffset = (i + RowId + vTexture.rows()) % vTexture.rows();
-					auto ColIdWithOffset = (k + ColId + vTexture.cols()) % vTexture.cols();
-
-					if (vNeighborMask(i + KernelOffset, k + KernelOffset) != 0)
-						Feature.emplace_back(vTexture.coeff(RowIdWithOffset, ColIdWithOffset));
-				}
-
-			//范数的值类型为Scalar_t，精度足够
-			auto Distance = (__flatFeature(Feature) - vFeature).squaredNorm();
+			auto Distance = __computeDistance(vFeature, __generateFeatureAt(vTexture, vNeighborMask, RowId, ColId));
+			
 			if (MinDistance > Distance)
 			{
 				MinDistance = Distance;
