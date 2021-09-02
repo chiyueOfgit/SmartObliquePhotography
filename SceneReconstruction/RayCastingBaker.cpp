c37d524081f97dc9a1b832a1a15f606819071340
bowen tan <t519803253@163.com>
2021-09-02 19:27:56 +0800

    add texturebaker

---
+++ SceneReconstruction/RayCastingBaker.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "RayCastingBaker.h"
+
+using namespace hiveObliquePhotography::SceneReconstruction;
+
+//REGISTER_NORMAL_PRODUCT(CPoissonSurfaceReconstructor, KEYWORD::POISSON_RECONSTRUCTOR)
+
+//*****************************************************************
+//FUNCTION: 
+hiveObliquePhotography::CImage<Eigen::Vector3i> CRayCastingBaker::bakeTexture(PointCloud_t::Ptr vPointCloud)
+{
+	
+}
---
+++ SceneReconstruction/TextureBaker.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "TextureBaker.h"
+
+using namespace hiveObliquePhotography::SceneReconstruction;
+
+//*****************************************************************
+//FUNCTION: 
+bool ITextureBaker::onProductCreatedV(const hiveConfig::CHiveConfig* vConfig, const CMesh& vMesh)
+{
+	
+}
