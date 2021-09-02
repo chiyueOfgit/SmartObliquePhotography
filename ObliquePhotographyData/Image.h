aa06f69ec1ff97c201b2e8c20ddaf25f02494139
李文峰 <2380945849@qq.com>
2021-09-02 10:09:09 +0800

    template CImage

--- ObliquePhotographyData/Image.h
+++ ObliquePhotographyData/Image.h
@@ -3,27 +3,22 @@
 
 namespace hiveObliquePhotography
 {
-	using TColor = Eigen::Vector3f;
+	template <typename TColor>
 	class CImage
 	{
 	public:
-		unsigned m_Height = 0;
-		unsigned m_Width = 0;
-		
 		CImage() = default;
 		~CImage() = default;
 
-		[[nodiscard]] auto getHeight() const { return m_Height; }
-		[[nodiscard]] auto getWidth() const { return m_Width; }
-		TColor& fetchColor(unsigned vRowId, unsigned vColId) { return m_Data(vRowId, vColId); }
-		const TColor& getColor(unsigned vRowId, unsigned vColId) const { return m_Data(vRowId, vColId); }
+		[[nodiscard]] auto getHeight() const { return m_Data.rows(); }
+		[[nodiscard]] auto getWidth() const { return m_Data.cols(); }
+		
+		[[nodiscard]] TColor& fetchColor(unsigned vRowId, unsigned vColId) { return m_Data(vRowId, vColId); }
+		[[nodiscard]] const TColor& getColor(unsigned vRowId, unsigned vColId) const { return m_Data(vRowId, vColId); }
+		
 		void fillColor(unsigned vHeight, unsigned vWidth, TColor* vBuffer)
 		{
-			m_Height = vHeight;
-			m_Width = vWidth;
-
-			m_Data.resize(m_Height, m_Width);
-			std::memcpy(m_Data.data(), vBuffer, m_Data.size() * sizeof(TColor));
+			m_Data = Eigen::Map<Eigen::Matrix<TColor, -1, -1>>(vBuffer, vHeight, vWidth);
 		}
 	
 	private:
