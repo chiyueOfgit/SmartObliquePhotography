c53dc77bf195c4322e3cfba20176ec427de821e6
Medjed <18181362690@163.com>
2021-09-03 10:53:45 +0800

    fix

--- ObliquePhotographyData/Image.h
+++ ObliquePhotographyData/Image.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "pch.h"
-#include "Mesh.h"
 
 namespace hiveObliquePhotography
 {
--- SceneReconstruction/RayCastingBaker.cpp
+++ SceneReconstruction/RayCastingBaker.cpp
@@ -9,26 +9,26 @@ _REGISTER_NORMAL_PRODUCT(CRayCastingBaker, KEYWORD::RAYCASTING_TEXTUREBAKER)
 //FUNCTION: 
 hiveObliquePhotography::CImage<Eigen::Vector3i> CRayCastingBaker::bakeTexture(PointCloud_t::Ptr vPointCloud)
 {
-	
+	return {};
 }
 
 //*****************************************************************
 //FUNCTION: 
 std::vector<STexelInfo> CRayCastingBaker::findTexelsPerFace(const SFace& vFace, Eigen::Vector2i vResolution)
 {
-
+	return {};
 }
 
 //*****************************************************************
 //FUNCTION: 
 std::vector<SCandidateInfo> CRayCastingBaker::executeIntersection(const STexelInfo& vInfo)
 {
-
+	return {};
 }
 
 //*****************************************************************
 //FUNCTION: 
 Eigen::Vector3i CRayCastingBaker::calcTexelColor(const std::vector<SCandidateInfo>& vCandidates)
 {
-
+	return {};
 }
