862ac09f7d72b400033c30531a4e2313aa6470bd
Medjed <18181362690@163.com>
2021-09-17 20:27:06 +0800

    .

--- UnitTests/Unit_Test_026/test.cpp
+++ UnitTests/Unit_Test_026/test.cpp
@@ -4,6 +4,8 @@
 
 #include <pcl/io/obj_io.h>
 #include <pcl/io/vtk_lib_io.h>
+#include <fstream>
+#include <regex>
 
 //测试用例列表：
 //   * findBoundaryPoint: 测试在简单场景下寻找边界点正确性。
@@ -29,11 +31,10 @@ protected:
 
 	hiveObliquePhotography::CMesh _loadMesh(const std::string& vPath)
 	{
-		pcl::TextureMesh TexMesh1, TexMesh2;
-		pcl::io::loadOBJFile(vPath, TexMesh1);
-		pcl::io::loadPolygonFileOBJ(vPath, TexMesh2);
-		TexMesh2.tex_materials = TexMesh1.tex_materials;
-		hiveObliquePhotography::CMesh Mesh(TexMesh1);
+		pcl::TextureMesh TexMesh;
+		pcl::io::loadOBJFile(vPath, TexMesh);
+		m_Material = TexMesh.tex_materials[0];
+		hiveObliquePhotography::CMesh Mesh(TexMesh);
 		bool EmptyFlag = Mesh.m_Vertices.empty() || Mesh.m_Faces.empty();
 		EXPECT_FALSE(EmptyFlag);
 		if (EmptyFlag)
@@ -51,6 +52,7 @@ protected:
 	}
 
 	hiveObliquePhotography::CMesh m_Mesh;
+	pcl::TexMaterial m_Material;
 
 	CArapParameterization* m_pMeshParameterization = nullptr;
 };
@@ -68,5 +70,37 @@ TEST_F(TestArapParameterization, TestfindBoundaryPoint)
 		m_Mesh.m_Vertices[Row].u = UV.row(Row)(0);
 		m_Mesh.m_Vertices[Row].v = UV.row(Row)(1);
 	}
-	int i = 0;
+
+	pcl::io::saveOBJFile("Plane.obj", m_Mesh.toTexMesh(m_Material));
+	std::fstream ObjFile("Plane.obj");
+	if (ObjFile.is_open())
+	{
+		std::string Line;
+		while (std::getline(ObjFile, Line))
+		{
+			if (Line[0] == 'f')
+			{
+				std::string FixedLine("f ");
+
+				std::smatch Result;
+				std::regex FaceRegex("\\d+/\\d+/\\d+");
+				for (auto Begin = Line.cbegin(); std::regex_search(Begin, Line.cend(), Result, FaceRegex); Begin = Result.suffix().first)
+				{
+					auto FaceStr = Result[0].str();
+					auto FirstPartition = FaceStr.find('/');
+					auto SecondPartition = FaceStr.find_last_of('/');
+					auto Vp = FaceStr.substr(0, FirstPartition);
+					auto Vt = FaceStr.substr(FirstPartition + 1, SecondPartition - FirstPartition - 1);
+					auto Vn = FaceStr.substr(SecondPartition + 1, FaceStr.length());
+
+					int i = 0;
+				}
+			}
+
+		}
+
+
+
+	}
+
 }
