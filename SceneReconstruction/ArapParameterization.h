f6f9f7bb16741ad001ac83db6980eebc3414ebb6
Medjed <18181362690@163.com>
2021-09-17 12:13:48 +0800

    add interface

--- SceneReconstruction/ArapParameterization.h
+++ SceneReconstruction/ArapParameterization.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "MeshParameterization.h"
+#include <Eigen/src/SparseCore/SparseMatrix.h>
 
 namespace hiveObliquePhotography
 {
@@ -28,7 +29,15 @@ namespace hiveObliquePhotography
 			void buildHalfEdge();
 			std::vector<int> findBoundaryPoint();
 			
+			Eigen::MatrixXd calcInitialUV(const CMesh& vMesh);
+
 		private:
+			void __fillTutteSolveMatrix(Eigen::SparseMatrix<double, Eigen::ColMajor, int>& vMatrix, const CMesh& vMesh);
+			void __fillTutteSolveVectors(Eigen::VectorXd& vVectorU, Eigen::VectorXd& vVectorV, const CMesh& vMesh);
+			Eigen::VectorXd __solveTutteEmbedding(const Eigen::SparseMatrix<double, Eigen::ColMajor, int>& vMatrix, const Eigen::VectorXd& vVector);
+
+			Eigen::MatrixXd __solveARAP(const Eigen::MatrixXd& vVertexPos, const Eigen::MatrixXi& vFaces, Eigen::MatrixXd& vInitialUV);
+
 			std::vector<SVertexInfo> m_VertexInfoTable;
 			std::vector<SHalfEdge> m_HalfEdgeTable;
 		};
