3f382abd520428a1a6a998800534cff325b21f93
Medjed <18181362690@163.com>
2021-10-08 17:31:22 +0800

    fix data again

--- ObliquePhotographyData/MeshOBJLoader.cpp
+++ ObliquePhotographyData/MeshOBJLoader.cpp
@@ -12,6 +12,9 @@ _REGISTER_EXCLUSIVE_PRODUCT(CMeshOBJLoader, OBJ_LOADER)
 int CMeshOBJLoader::__loadDataFromFileV(const std::string& vFileName, CMesh& voMesh)
 {
 	hiveObliquePhotography::CMesh Mesh;
+	pcl::TexMaterial Material;
+
+	auto FolderPath = vFileName.substr(0, vFileName.find_last_of("/") + 1);
 
 	std::ifstream ObjFileIn(vFileName);
 	if (ObjFileIn.is_open())
@@ -19,54 +22,33 @@ int CMeshOBJLoader::__loadDataFromFileV(const std::string& vFileName, CMesh& voM
 		std::string Line;
 		std::vector<Eigen::Vector3f> Normals;
 		std::vector<Eigen::Vector2f> TexCoords;
+
 		std::string NumberRegex = "(-?\\d+.?\\d+e?-?\\d*)";
 		std::string SpaceRegex = " +";
-		while (std::getline(ObjFileIn, Line))
+
+		std::regex ThreeNumRegex(NumberRegex + SpaceRegex + NumberRegex + SpaceRegex + NumberRegex);
+		std::regex TwoNumRegex(NumberRegex + SpaceRegex + NumberRegex);
+		std::regex FaceRegex("(\\d*)/(\\d*)/(\\d*)");
+		std::smatch Result;
+
+		while (std::getline(ObjFileIn, Line) && Line[0] != 'v')
 		{
 			if (Line[0] == '#')
 				continue;
-			if (Line.find("mtllib") != std::string::npos)
+			if (Line.find("mtllib") != std::string::npos)	// load mtl
 			{
+				std::string MtlPath;
+				if (Line.find("./") != std::string::npos)
+					MtlPath = FolderPath + Line.substr(Line.find('/') + 1, Line.length());
+				else
+					MtlPath = FolderPath + Line.substr(Line.find(' ') + 1, Line.length());
+				Material = __loadMaterialFromFile(MtlPath);
 			}
-			if (Line.substr(0, 2) == "v ")
-			{
-				std::smatch Result;
-				std::regex VertexRegex(NumberRegex + SpaceRegex + NumberRegex + SpaceRegex + NumberRegex);
-				std::regex_search(Line, Result, VertexRegex);
-				_ASSERTE(Result.size() == 4);
-				hiveObliquePhotography::SVertex Vertex;
-				Vertex.x = std::stod(Result[1]);
-				Vertex.y = std::stod(Result[2]);
-				Vertex.z = std::stod(Result[3]);
-				Mesh.m_Vertices.push_back(Vertex);
-			}
-			if (Line.substr(0, 2) == "vt")
-			{
-				std::smatch Result;
-				std::regex VertexRegex(NumberRegex + SpaceRegex + NumberRegex);
-				std::regex_search(Line, Result, VertexRegex);
-				_ASSERTE(Result.size() == 3);
-				Eigen::Vector2f TexCoord;
-				TexCoord.x() = std::stod(Result[1]);
-				TexCoord.y() = std::stod(Result[2]);
-				TexCoords.push_back(TexCoord);
-			}
-			if (Line.substr(0, 2) == "vn")
-			{
-				std::smatch Result;
-				std::regex VertexRegex(NumberRegex + SpaceRegex + NumberRegex + SpaceRegex + NumberRegex);
-				std::regex_search(Line, Result, VertexRegex);
-				_ASSERTE(Result.size() == 4);
-				Eigen::Vector3f Normal;
-				Normal.x() = std::stod(Result[1]);
-				Normal.y() = std::stod(Result[2]);
-				Normal.z() = std::stod(Result[3]);
-				Normals.push_back(Normal);
-			}
+		}
+		do
+		{
 			if (Line[0] == 'f')
 			{
-				std::smatch Result;
-				std::regex FaceRegex("(\\d*)/(\\d*)/(\\d*)");
 				std::vector<uint32_t> Face;
 				for (auto Begin = Line.cbegin(); std::regex_search(Begin, Line.cend(), Result, FaceRegex); Begin = Result.suffix().first)
 				{
@@ -96,12 +78,106 @@ int CMeshOBJLoader::__loadDataFromFileV(const std::string& vFileName, CMesh& voM
 
 				Mesh.m_Faces.push_back({ Face[0], Face[1], Face[2] });
 			}
-		}
+			else if (Line.substr(0, 2) == "v ")
+			{
+				std::regex_search(Line, Result, ThreeNumRegex);
+				_ASSERTE(Result.size() == 4);
+				hiveObliquePhotography::SVertex Vertex;
+				Vertex.x = std::stod(Result[1]);
+				Vertex.y = std::stod(Result[2]);
+				Vertex.z = std::stod(Result[3]);
+				Mesh.m_Vertices.push_back(Vertex);
+			}
+			else if (Line.substr(0, 2) == "vt")
+			{
+				std::regex_search(Line, Result, TwoNumRegex);
+				_ASSERTE(Result.size() == 3);
+				Eigen::Vector2f TexCoord;
+				TexCoord.x() = std::stod(Result[1]);
+				TexCoord.y() = std::stod(Result[2]);
+				TexCoords.push_back(TexCoord);
+			}
+			else if (Line.substr(0, 2) == "vn")
+			{
+				std::regex_search(Line, Result, ThreeNumRegex);
+				_ASSERTE(Result.size() == 4);
+				Eigen::Vector3f Normal;
+				Normal.x() = std::stod(Result[1]);
+				Normal.y() = std::stod(Result[2]);
+				Normal.z() = std::stod(Result[3]);
+				Normals.push_back(Normal);
+			}
+		} while (std::getline(ObjFileIn, Line));
 
 		ObjFileIn.close();
+		if (Material.tex_name != "")
+			Mesh.setMaterial(Material);
 		voMesh = Mesh;
 		return 0;
 	}
 	else
 		return -1;
+}
+
+pcl::TexMaterial CMeshOBJLoader::__loadMaterialFromFile(const std::string& vFileName)
+{
+	pcl::TexMaterial Material;
+
+	std::string Line;
+
+	std::string NumberRegex = "(-?\\d+.?\\d+e?-?\\d*)";
+	std::string SpaceRegex = " +";
+
+	std::regex ThreeNumRegex(NumberRegex + SpaceRegex + NumberRegex + SpaceRegex + NumberRegex);
+	std::smatch Result;
+
+	std::ifstream MtlFileIn(vFileName);
+	if (MtlFileIn.is_open())
+		while (std::getline(MtlFileIn, Line))
+		{
+			if (Line.find("newmtl ") != std::string::npos)
+				Material.tex_name = Line.substr(Line.find(' ') + 1, Line.length());
+			if (Line.find("map_Kd") != std::string::npos)
+			{
+				std::string MapPath;
+				auto FolderPath = vFileName.substr(0, vFileName.find_last_of("/") + 1);
+				if (Line.find("./") != std::string::npos)
+					MapPath = FolderPath + Line.substr(Line.find('/') + 1, Line.length());
+				else
+					MapPath = FolderPath + Line.substr(Line.find(' ') + 1, Line.length());
+				Material.tex_file = MapPath;
+			}
+			if (Line.find("Ns ") != std::string::npos)
+				Material.tex_Ns = std::stof(Line.substr(Line.find(' ') + 1, Line.length()));
+			if (Line.find("d ") != std::string::npos && Line.find("Kd") == std::string::npos)
+				Material.tex_d = std::stof(Line.substr(Line.find(' ') + 1, Line.length()));
+			if (Line.find("illum") != std::string::npos)
+				Material.tex_illum = std::stoi(Line.substr(Line.find(' ') + 1, Line.length()));
+			if (Line.find("Ka ") != std::string::npos)
+			{
+				std::smatch Result;
+				std::regex VertexRegex(NumberRegex + SpaceRegex + NumberRegex + SpaceRegex + NumberRegex);
+				std::regex_search(Line, Result, VertexRegex);
+				_ASSERTE(Result.size() == 4);
+				Material.tex_Ka = { std::stof(Result[1]), std::stof(Result[2]), std::stof(Result[3]) };
+			}
+			if (Line.find("Kd ") != std::string::npos && Line.find("map_Kd") == std::string::npos)
+			{
+				std::smatch Result;
+				std::regex VertexRegex(NumberRegex + SpaceRegex + NumberRegex + SpaceRegex + NumberRegex);
+				std::regex_search(Line, Result, VertexRegex);
+				_ASSERTE(Result.size() == 4);
+				Material.tex_Kd = { std::stof(Result[1]), std::stof(Result[2]), std::stof(Result[3]) };
+			}
+			if (Line.find("Ks ") != std::string::npos)
+			{
+				std::smatch Result;
+				std::regex VertexRegex(NumberRegex + SpaceRegex + NumberRegex + SpaceRegex + NumberRegex);
+				std::regex_search(Line, Result, VertexRegex);
+				_ASSERTE(Result.size() == 4);
+				Material.tex_Ks = { std::stof(Result[1]), std::stof(Result[2]), std::stof(Result[3]) };
+			}
+		}
+
+	return Material;
 }
