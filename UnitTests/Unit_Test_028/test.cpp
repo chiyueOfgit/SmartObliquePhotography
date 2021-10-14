02d348ffc997f9f1357a58d4b35797588ae28a9e
Moye-mio <1154268595@qq.com>
2021-10-14 19:59:43 +0800

    merge

--- UnitTests/Unit_Test_028/test.cpp
+++ UnitTests/Unit_Test_028/test.cpp
@@ -6,8 +6,9 @@
 
 //测试用例列表：
 //  * Test_NoHoles: 判断网格是否有洞
-//	* Test_NoFlips: 判断网格是否有面片翻转
+//	* Test_NoFlips: 判断网格是否有面片翻转（如果某面片的法向量与其它面片不一样，则发生翻转）
 //	* Test_NoDuplicates: 判断网格是否有面片重复
+//	* Test_SelfIntersections: 判断网格是否有面片重叠
 
 using namespace hiveObliquePhotography::SceneReconstruction;
 
@@ -27,44 +28,13 @@ protected:
 		toVcgMesh(m_Mesh, m_VcgMesh);
 	}
 
-	void LoadTwoMeshes(const std::string& vLhsPath, const std::string& vRhsPath)
-	{
-		//hiveObliquePhotography::hiveLoadMeshModel(m_LhsMesh, vLhsPath);
-		//hiveObliquePhotography::hiveLoadMeshModel(m_RhsMesh, vRhsPath);
-
-		////合并为一个VcgMesh
-		//m_VcgMesh.Clear();
-
-		//auto VertexIterator = vcg::tri::Allocator<hiveObliquePhotography::CVcgMesh>::AddVertices(m_VcgMesh, m_LhsMesh.m_Vertices.size());
-		//for (auto& Vertex : m_LhsMesh.m_Vertices)
-		//{
-		//	VertexIterator->P() = { Vertex.x, Vertex.y, Vertex.z };
-		//	VertexIterator->N() = { Vertex.nx, Vertex.ny, Vertex.nz };
-		//	VertexIterator->T() = { Vertex.u, Vertex.v };
-		//	++VertexIterator;
-		//}
-
-		//auto FaceIterator = vcg::tri::Allocator<hiveObliquePhotography::CVcgMesh>::AddFaces(m_VcgMesh, m_LhsMesh.m_Faces.size());
-		//for (auto& Face : m_LhsMesh.m_Faces)
-		//{
-		//	FaceIterator->V(0) = &m_VcgMesh.vert.at(Face.a);
-		//	FaceIterator->V(1) = &m_VcgMesh.vert.at(Face.b);
-		//	FaceIterator->V(2) = &m_VcgMesh.vert.at(Face.c);
-		//	++FaceIterator;
-		//}
-
-		//vcg::tri::UpdateTopology<hiveObliquePhotography::CVcgMesh>::FaceFace(m_VcgMesh);
-	}
-
 	hiveObliquePhotography::CMesh m_Mesh;
-	hiveObliquePhotography::CMesh m_LhsMesh;
-	hiveObliquePhotography::CMesh m_RhsMesh;
 	hiveObliquePhotography::CVcgMesh m_VcgMesh;
 };
 
 TEST_F(TestMeshLegality, Test_NoHoles)
 {
-	LoadMesh(LhsMeshPath);
+	LoadMesh(RhsMeshPath);
 	int HolesCount = vcg::tri::Clean<hiveObliquePhotography::CVcgMesh>::CountHoles(m_VcgMesh);
 	EXPECT_EQ(HolesCount, 0);
 }
@@ -81,4 +51,13 @@ TEST_F(TestMeshLegality, Test_NoDuplicates)
 	LoadMesh(LhsMeshPath);
 	int OverlapsCount = vcg::tri::Clean<hiveObliquePhotography::CVcgMesh>::RemoveDuplicateFace(m_VcgMesh);
 	EXPECT_EQ(OverlapsCount, 0);
+}
+
+TEST_F(TestMeshLegality, Test_SelfIntersections)
+{
+	LoadMesh(LhsMeshPath);
+	std::vector<hiveObliquePhotography::SVcgFace*> IntersectionFaces;
+	vcg::tri::Clean<hiveObliquePhotography::CVcgMesh>::SelfIntersections(m_VcgMesh, IntersectionFaces);
+	int SelfIntersectionsCount = IntersectionFaces.size();
+	EXPECT_EQ(SelfIntersectionsCount, 0);
 }
