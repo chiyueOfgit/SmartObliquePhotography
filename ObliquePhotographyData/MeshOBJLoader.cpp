b008f2ddb621434102101ef3ea2e6fd36ec7b5ea
李文峰 <2380945849@qq.com>
2021-10-15 11:20:36 +0800

    CMeshOBJLoader 支持相对路径

--- ObliquePhotographyData/MeshOBJLoader.cpp
+++ ObliquePhotographyData/MeshOBJLoader.cpp
@@ -12,19 +12,30 @@ _REGISTER_EXCLUSIVE_PRODUCT(CMeshOBJLoader, OBJ_LOADER)
 //FUNCTION:
 int CMeshOBJLoader::__loadDataFromFileV(const std::string& vFileName, CMesh& voMesh)
 {
+	auto Split = vFileName.find_last_of("/") + 1;
+	auto FolderPath = vFileName.substr(0, Split);
+	auto PureName = vFileName.substr(Split);
+	
+	WCHAR CurrentDirectory[MAX_PATH] = { 0 };
+	GetCurrentDirectory(MAX_PATH, CurrentDirectory);
+	SetCurrentDirectoryA(FolderPath.c_str());
+
 	CVcgMesh VcgMesh;
 	auto ImportInfo = vcg::tri::io::ImporterOBJ<CVcgMesh>::Info();
-	auto ErrorCode = vcg::tri::io::ImporterOBJ<CVcgMesh>::Open(VcgMesh, vFileName.c_str(), ImportInfo);
+	auto ErrorCode = vcg::tri::io::ImporterOBJ<CVcgMesh>::Open(VcgMesh, PureName.c_str(), ImportInfo);
 	fromVcgMesh(VcgMesh, voMesh);
-
-	//CVcgMesh::PerFaceAttributeHandle<int> mIndHandle =
-	//	vcg::tri::Allocator<CVcgMesh>::GetPerFaceAttribute<int>(VcgMesh, std::string("materialIndex"));
-	//auto material_index = mIndHandle;
+	SetCurrentDirectory(CurrentDirectory);
 
 	CVcgMesh::PerMeshAttributeHandle<std::vector<vcg::tri::io::Material>> materialsHandle =
 		vcg::tri::Allocator<CVcgMesh>::GetPerMeshAttribute<std::vector<vcg::tri::io::Material> >(VcgMesh, std::string("materialVector"));
 	if (!materialsHandle().empty())
-		voMesh.setMaterial(fromVcgMaterial(materialsHandle().front()));
+	{
+		auto Material = fromVcgMaterial(materialsHandle().front());
+		if (hiveUtility::hiveLocateFile(Material.tex_file).empty())
+			Material.tex_file.insert(0, FolderPath);
 
+		voMesh.setMaterial(Material);
+	}
+	
 	return ErrorCode;
 }
