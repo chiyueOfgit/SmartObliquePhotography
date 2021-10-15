d8de7950d9a5a15424a3bb87dd852e64c47df0d3
李文峰 <2380945849@qq.com>
2021-10-15 10:15:30 +0800

    .

--- ObliquePhotographyData/MeshOBJLoader.cpp
+++ ObliquePhotographyData/MeshOBJLoader.cpp
@@ -1,10 +1,8 @@
 #include "pch.h"
 #include "MeshOBJLoader.h"
-#include <regex>
 #include <wrap/io_trimesh/import_obj.h>
 #include <wrap/io_trimesh/io_material.h>
 #include "VcgMesh.hpp"
-#include "Mesh.h"
 
 using namespace hiveObliquePhotography;
 
@@ -14,9 +12,7 @@ _REGISTER_EXCLUSIVE_PRODUCT(CMeshOBJLoader, OBJ_LOADER)
 //FUNCTION:
 int CMeshOBJLoader::__loadDataFromFileV(const std::string& vFileName, CMesh& voMesh)
 {
-	//CMesh Mesh;
 	CVcgMesh VcgMesh;
-
 	auto ImportInfo = vcg::tri::io::ImporterOBJ<CVcgMesh>::Info();
 	auto ErrorCode = vcg::tri::io::ImporterOBJ<CVcgMesh>::Open(VcgMesh, vFileName.c_str(), ImportInfo);
 	fromVcgMesh(VcgMesh, voMesh);
@@ -27,7 +23,8 @@ int CMeshOBJLoader::__loadDataFromFileV(const std::string& vFileName, CMesh& voM
 
 	CVcgMesh::PerMeshAttributeHandle<std::vector<vcg::tri::io::Material>> materialsHandle =
 		vcg::tri::Allocator<CVcgMesh>::GetPerMeshAttribute<std::vector<vcg::tri::io::Material> >(VcgMesh, std::string("materialVector"));
+	if (!materialsHandle().empty())
+		voMesh.setMaterial(fromVcgMaterial(materialsHandle().front()));
 
-	voMesh.setMaterial(fromVcgMaterial(materialsHandle().front()));
 	return ErrorCode;
 }
