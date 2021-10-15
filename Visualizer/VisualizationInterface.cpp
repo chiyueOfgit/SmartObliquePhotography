252b6defb8eeed825048459690d816cce2990c71
Medjed <18181362690@163.com>
2021-10-15 10:31:15 +0800

    first

--- Visualizer/VisualizationInterface.cpp
+++ Visualizer/VisualizationInterface.cpp
@@ -121,9 +121,18 @@ void hiveObliquePhotography::Visualization::TestInterface(const CMesh& vMesh, co
 		Point.x = Vertex.x;
 		Point.y = Vertex.y;
 		Point.z = Vertex.z;
-		Point.r = 255 - Color;
-		Point.g = 0;
-		Point.b = Color;
+		if (Color == 0)
+		{
+			Point.r = 0;
+			Point.g = 255;
+			Point.b = 0;
+		}
+		else
+		{
+			Point.r = 255 - Color;
+			Point.g = 0;
+			Point.b = Color;
+		}
 		pCloud->push_back(Point);
 		Color += Step;
 	}
