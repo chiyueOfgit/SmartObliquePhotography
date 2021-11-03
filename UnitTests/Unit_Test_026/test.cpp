ad27fb881bf7596819dc37a84d34a60fc4cb9e2f
海嫣 王 <19808255263@163.com>
2021-11-04 00:17:41 +0800

    添加关于生成半边数据结构的测试用例；增加测试数据

--- UnitTests/Unit_Test_026/test.cpp
+++ UnitTests/Unit_Test_026/test.cpp
@@ -18,14 +18,16 @@ const auto StoneMeshPath = TESTMODEL_DIR + std::string("/Test026_Model/Others/LI
 const auto MountainMeshPath = TESTMODEL_DIR + std::string("/Test026_Model/Others/mountain.obj");
 const auto ScuMeshPath = TESTMODEL_DIR + std::string("/Test026_Model/Scu/Tile16.obj");
 const auto PyramidMeshPath = TESTMODEL_DIR + std::string("/Test026_Model/Pyramid.obj");
-const auto PoissonMeshPath = TESTMODEL_DIR + std::string("/Test026_Model/Tile1.obj");
+const auto PoissonMeshPath = TESTMODEL_DIR + std::string("/Test026_Model/Tile1.obj"); 
+const auto CubeMeshPath = TESTMODEL_DIR + std::string("/Test026_Model/Cube.obj");
+const auto CubeTestDataPath = TESTMODEL_DIR + std::string("/Test026_Model/CubeTestData.txt");
 
 class TestArapParameterization : public testing::Test
 {
 protected:
 	void SetUp() override
 	{
-		m_MeshPath = PlaneMeshPath;
+		m_MeshPath = CubeMeshPath;
 		_loadObj(m_MeshPath, m_Mesh);
 		ASSERT_TRUE(!m_Mesh.m_Vertices.empty());
 		m_pMeshParameterization = _createProduct(m_Mesh);
@@ -45,6 +47,81 @@ protected:
 	{
 		hiveObliquePhotography::hiveSaveMeshModel(vMesh, vPath);
 	}
+	int _stringToInt(const std::string vStringData)
+	{
+		std::stringstream StringToInt;
+		int IntData;
+		IntData = 0;
+		StringToInt << vStringData;
+		StringToInt >> IntData;
+		return IntData;
+	}
+
+	void _testHalfEdgeTable()
+	{
+		std::string ContentPerLine, s1, s2, s3, s4, s5;
+		//std::vector<std::vector<int>> VertexInfoTable;
+		std::vector<SHalfEdge> TestHalfEdgeTable;
+		TestHalfEdgeTable.resize(12 * 3);
+		std::ifstream inf;
+		inf.open(CubeMeshPath);
+
+		while (getline(inf, ContentPerLine))
+		{
+			std::istringstream in(ContentPerLine);
+			float VertexId = 0;
+			int vertexNum, edgeNum, FaceId, HalfEdgeId;
+			vertexNum = 0;
+			edgeNum = 0;
+			FaceId = 0;
+			HalfEdgeId = 0;
+
+			SHalfEdge TestHalfEdge;
+			in >> s1 >> s2 >> s3 >> s4 >> s5;
+			//TestHalfEdge._Face = FaceId;
+			TestHalfEdge._VertexId = _stringToInt(s1);
+			TestHalfEdge._Prev = _stringToInt(s2);
+			TestHalfEdge._Next = _stringToInt(s3);
+			TestHalfEdge._Conj = _stringToInt(s4);
+			TestHalfEdge._Face = _stringToInt(s5);
+			std::cout << TestHalfEdge._VertexId << " ";
+			std::cout << TestHalfEdge._Prev << " ";
+			std::cout << TestHalfEdge._Next << " ";
+			std::cout << TestHalfEdge._Conj << " ";
+			std::cout << TestHalfEdge._Face << std::endl;
+			FaceId++;
+			TestHalfEdgeTable.push_back(TestHalfEdge);
+			//while (in >> s1 >> s2 >> s3 >> s4 >> s5) {
+			//	SHalfEdge TestHalfEdge;
+			//	//TestHalfEdge._Face = FaceId;
+			//	TestHalfEdge._VertexId = _stringToInt(s1);
+			//	TestHalfEdge._Prev = _stringToInt(s2);
+			//	TestHalfEdge._Next = _stringToInt(s3);
+			//	TestHalfEdge._Conj = _stringToInt(s4);
+			//	TestHalfEdge._Face = _stringToInt(s5);
+			//	std::cout << TestHalfEdge._VertexId << " ";
+			//	std::cout << TestHalfEdge._Prev << " ";
+			//	std::cout << TestHalfEdge._Next << " ";
+			//	std::cout << TestHalfEdge._Conj << " ";
+			//	std::cout << TestHalfEdge._Face << " ";
+			//	FaceId++;
+			//	TestHalfEdgeTable.push_back(TestHalfEdge);
+			//}
+		}
+
+		inf.close();
+		/*EXPECT_EQ(m_HalfEdgeTable.size(), m_Mesh.m_Faces.size()*3);
+		for (int HalfEdgeId = 0; HalfEdgeId < m_HalfEdgeTable.size(); HalfEdgeId++)
+		{
+			EXPECT_EQ(1,1);
+		}
+		std::vector<std::vector<int>> m_VertexInfoTable;
+		m_VertexInfoTable.resize(8);
+		for (size_t VertexId = 0; VertexId < 8; VertexId++)
+		{
+			std::vector<int>
+		}*/
+	}
 
 	CArapParameterizer* _createProduct(const hiveObliquePhotography::CMesh& vMesh)
 	{
@@ -61,3 +138,9 @@ protected:
 
 	CArapParameterizer* m_pMeshParameterization = nullptr;
 };
+
+TEST_F(TestArapParameterization, Test_buildHalfEdgeTest)
+{
+	m_pMeshParameterization->buildHalfEdge();
+	_testHalfEdgeTable();
+}
